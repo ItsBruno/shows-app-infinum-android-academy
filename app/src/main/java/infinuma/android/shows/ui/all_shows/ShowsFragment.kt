5540fda0b6455ce2893022a86218a33873e99b5c
Bruno Mikan <brunomikan@gmail.com>
2023-07-21 13:18:50 +0200

    Add profile dialog

--- app/src/main/java/infinuma/android/shows/ui/all_shows/ShowsFragment.kt
+++ app/src/main/java/infinuma/android/shows/ui/all_shows/ShowsFragment.kt
@@ -1,6 +1,8 @@
 package infinuma.android.shows.ui.all_shows
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,10 +11,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.edit
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import infinuma.android.shows.R
+import infinuma.android.shows.databinding.DialogProfileBinding
 import infinuma.android.shows.databinding.FragmentShowsBinding
 import infinuma.android.shows.model.shows
 import infinuma.android.shows.ui.login.LoginFragment
+import infinuma.android.shows.ui.login.LoginFragment.Companion.USER_EMAIL
 import infinuma.android.shows.ui.login.PREFERENCES_NAME
 
 class ShowsFragment : Fragment() {
@@ -57,16 +62,47 @@ class ShowsFragment : Fragment() {
                 toggleRecyclerView()
             }
 
-            logoutButton.setOnClickListener {
-                //forget user
-                sharedPreferences.edit {
-                    putBoolean(LoginFragment.REMEMBER_USER, false)
-                    putString(LoginFragment.USER_EMAIL, null)
-                    putString(LoginFragment.USER_PASSWORD, null)
-                }
-                findNavController().navigate(ShowsFragmentDirections.actionShowDetailsFragmentToLoginFragment())
+            profile.setOnClickListener {
+                buildProfileBottomSheetDialog()
             }
         }
+    }
+
+    private fun buildProfileBottomSheetDialog() {
+        val dialog = BottomSheetDialog(requireContext())
+        val dialogProfileBinding = DialogProfileBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogProfileBinding.root)
+
+        with(dialogProfileBinding) {
+            userEmail.text = sharedPreferences.getString(USER_EMAIL, "")
+            changeProfilePictureButton.setOnClickListener {
+                //TODO add open camera and select from gallery functionality
+            }
+
+            logoutButton.setOnClickListener {
+                initLogoutAlertDialog(dialog)
+            }
+        }
+        dialog.show()
+    }
+
+    private fun initLogoutAlertDialog(dialog: BottomSheetDialog) {
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.confirm_logout))
+            .setMessage(R.string.logout_confirmation_message)
+            .setNegativeButton(R.string.no, null)
+            .setPositiveButton(R.string.yes, DialogInterface.OnClickListener { _, _ -> logoutUser(dialog) })
+            .show()
+    }
+
+    private fun logoutUser(dialog: BottomSheetDialog) {
+        sharedPreferences.edit {
+            putBoolean(LoginFragment.REMEMBER_USER, false)
+            putString(LoginFragment.USER_EMAIL, null)
+            putString(LoginFragment.USER_PASSWORD, null)
+        }
+        dialog.dismiss()
+        findNavController().navigate(ShowsFragmentDirections.actionShowDetailsFragmentToLoginFragment())
     }
 
     private fun toggleRecyclerView() {
