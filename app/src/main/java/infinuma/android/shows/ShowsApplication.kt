0a2772404f819cb1211709641b9660c420710996
Bruno Mikan <brunomikan@gmail.com>
2023-08-06 10:43:35 +0200

    Change buttons styles

--- app/src/main/java/infinuma/android/shows/ShowsApplication.kt
+++ app/src/main/java/infinuma/android/shows/ShowsApplication.kt
@@ -2,7 +2,6 @@ package infinuma.android.shows
 
 import android.app.Application
 import infinuma.android.shows.db.ShowsDatabase
-import infinuma.android.shows.networking.ApiModule.initRetrofit
 
 class ShowsApplication : Application() {
     val database by lazy {
